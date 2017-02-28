f1d61349c818ebd7cb43e6b5aff44a98986490c8
Tarek Kalaji <tarek-aec@hotmail.com>
2017-02-28 03:15:34 +0200

    modified logout function

--- main/apps/account/urls.py
+++ main/apps/account/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views.login import login_view
+from .views.login import login_view, logout_view
 from .views.dashboard import dashboard_view
 
 urlpatterns = [
     url(r'^account/$', login_view, name='login_view'),
+    url(r'^logout/$', view=logout_view, name='logout',),
     url(r'^dashboard/$', dashboard_view, name='dashboard_view'),
 ]
