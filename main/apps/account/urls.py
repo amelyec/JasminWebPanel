67961d3d0fccf02c96b682be99b30072c8653b1c
Tarek Kalaji <tarek-aec@hotmail.com>
2017-02-28 03:16:38 +0200

    Update urls.py

--- main/apps/account/urls.py
+++ main/apps/account/urls.py
@@ -4,6 +4,6 @@ from .views.dashboard import dashboard_view
 
 urlpatterns = [
     url(r'^account/$', login_view, name='login_view'),
-    url(r'^logout/$', view=logout_view, name='logout',),
+    url(r'^logout/$', view=logout_view, name='logout_view',),
     url(r'^dashboard/$', dashboard_view, name='dashboard_view'),
 ]
