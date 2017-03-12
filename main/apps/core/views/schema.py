dd2371dee28222c4a69b6004bb9147206ad3c31d
Tarek Kalaji <tarek-aec@hotmail.com>
2017-03-12 13:01:06 +0200

    Update schema.py

--- main/apps/core/views/schema.py
+++ main/apps/core/views/schema.py
@@ -1,11 +1,11 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.schemas import SchemaGenerator
-from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework_swagger import renderers
 
 
-class SwaggerSchemaView(APIView):
+class SwaggerSchemaView(ViewSet):
     permission_classes = [AllowAny]
     renderer_classes = [
         renderers.OpenAPIRenderer,
