f6dd27c7bc28d37a4d195d2a3b3f413e230d38b9
Tarek Kalaji <tarek-aec@hotmail.com>
2017-03-12 13:03:13 +0200

    Update morouter.py

--- main/apps/core/views/morouter.py
+++ main/apps/core/views/morouter.py
@@ -15,8 +15,9 @@ from main.apps.core.exceptions import (JasminSyntaxError, JasminError,
 STANDARD_PROMPT = settings.STANDARD_PROMPT
 INTERACTIVE_PROMPT = settings.INTERACTIVE_PROMPT
 
+from .schema import SwaggerSchemaView
 
-class MORouterViewSet(ViewSet):
+class MORouterViewSet(SwaggerSchemaView):
     "Viewset for managing MO Routes"
     lookup_field = 'order'
 
